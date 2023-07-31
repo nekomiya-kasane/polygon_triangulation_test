70b826958f5855b1e41d19aae68ed467aa387ff2
nekomiya-kasane <takenoko1997@foxmail.com>
2023-07-31 17:32:53 +0800

    fixed caase BoatCCWMergeRight2SameY

--- tests/basic.cpp
+++ tests/basic.cpp
@@ -98,8 +98,9 @@ TEST(BasicTest, DiamondCWMergeRight)
   EXPECT_EQ(results.size(), 2);
 }
 
-void TestBoatCCWMergeRight(const Vec2Set &points)
+TEST(BasicTest, BoatCCWMergeRight)
 {
+  Vec2Set points = {{0, 2}, {0, 1}, {-1, 0}, {4, -4}, {4, -1}};
   Triangulator triangulator;
   triangulator.config.useGivenSeed = true;
   triangulator.config.seed         = 1;
@@ -204,16 +205,109 @@ void TestBoatCCWMergeRight(const Vec2Set &points)
   EXPECT_EQ(results.size(), 3);
 }
 
-TEST(BasicTest, BoatCCWMergeRight)
-{
-  Vec2Set points = {{0, 2}, {0, 1}, {-1, 0}, {4, -4}, {4, -1}};
-  TestBoatCCWMergeRight(points);
-}
-
 TEST(BasicTest, BoatCCWMergeRight2SameY)
 {
   Vec2Set points = {{0, 2}, {0, 1}, {-1, 0}, {4, 0}, {4, 1}};
-  TestBoatCCWMergeRight(points);
+  Triangulator triangulator;
+  triangulator.config.useGivenSeed = true;
+  triangulator.config.seed         = 1;
+  triangulator.AddPolygon(points, true);
+  triangulator.Build();
+
+  // check size
+  EXPECT_EQ(triangulator._nodes.Size(), 23);
+  EXPECT_EQ(triangulator._vertices.Size(), 5);
+  EXPECT_EQ(triangulator._segments.Size(), 5);
+  EXPECT_EQ(triangulator._regions.Size(), 11);
+  EXPECT_EQ(triangulator._prevVertices.Size(), 5);
+  EXPECT_EQ(triangulator._endVertices.Size(), 5);
+
+  // check the trapezoid map
+  const auto &node0 = triangulator._nodes[0];
+  EXPECT_NODE(node0, 0, Node::VERTEX, 4, 2, 1);
+  const auto &node1 = triangulator._nodes[1];
+  EXPECT_NODE(node1, 1, Node::VERTEX, 3, 4, 3);
+  const auto &node2 = triangulator._nodes[2];
+  EXPECT_NODE(node2, 2, Node::VERTEX, 1, 8, 7);
+  const auto &node3 = triangulator._nodes[3];
+  EXPECT_NODE(node3, 3, Node::REGION, 2, Nil, Nil);
+  const auto &node4 = triangulator._nodes[4];
+  EXPECT_NODE(node4, 4, Node::SEGMENT, 3, 6, 5);
+  const auto &node5 = triangulator._nodes[5];
+  EXPECT_NODE(node5, 5, Node::REGION, 3, Nil, Nil);
+  const auto &node6 = triangulator._nodes[6];
+  EXPECT_NODE(node6, 6, Node::VERTEX, 2, 10,9);
+  const auto &node7 = triangulator._nodes[7];
+  EXPECT_NODE(node7, 7, Node::SEGMENT, 1, 12, 11);
+  const auto &node8 = triangulator._nodes[8];
+  EXPECT_NODE(node8, 8, Node::VERTEX, 0, 17, 16);
+  const auto &node9 = triangulator._nodes[9];
+  EXPECT_NODE(node9, 9, Node::SEGMENT, 2, 15, 14);
+  const auto &node10 = triangulator._nodes[10];
+  EXPECT_NODE(node10, 10, Node::SEGMENT, 1, 12, 13);
+  const auto &node11 = triangulator._nodes[11];
+  EXPECT_NODE(node11, 11, Node::SEGMENT, 4, 20, 18);
+  const auto &node12 = triangulator._nodes[12];
+  EXPECT_NODE(node12, 12, Node::REGION, 4, Nil, Nil);
+  const auto &node13 = triangulator._nodes[13];
+  EXPECT_NODE(node13, 13, Node::REGION, 1, Nil, Nil);
+  const auto &node14 = triangulator._nodes[14];
+  EXPECT_NODE(node14, 14, Node::REGION, 7, Nil, Nil);
+  const auto &node15 = triangulator._nodes[15];
+  EXPECT_NODE(node15, 15, Node::REGION, 5, Nil, Nil);
+  const auto &node16 = triangulator._nodes[16];
+  EXPECT_NODE(node16, 16, Node::SEGMENT, 4, 19, 18);
+  const auto &node17 = triangulator._nodes[17];
+  EXPECT_NODE(node17, 17, Node::REGION, 0, Nil, Nil);
+  const auto &node18 = triangulator._nodes[18];
+  EXPECT_NODE(node18, 18, Node::REGION, 9, Nil, Nil);
+  const auto &node19 = triangulator._nodes[19];
+  EXPECT_NODE(node19, 19, Node::SEGMENT, 1, 22, 21);
+  const auto &node20 = triangulator._nodes[20];
+  EXPECT_NODE(node20, 20, Node::REGION, 6, Nil, Nil);
+  const auto &node21 = triangulator._nodes[21];
+  EXPECT_NODE(node21, 21, Node::REGION, 10, Nil, Nil);
+  const auto &node22 = triangulator._nodes[22];
+  EXPECT_NODE(node22, 22, Node::REGION, 8, Nil, Nil);
+
+  // check low neighbors of vertices
+  const auto &n0 = triangulator._lowNeighbors[0];
+  EXPECT_LOW_NEIGHBOR(n0, 8, 10, 9);
+  const auto &n1 = triangulator._lowNeighbors[1];
+  EXPECT_LOW_NEIGHBOR(n1, 4, Nil, 6);
+  const auto &n2 = triangulator._lowNeighbors[2];
+  EXPECT_LOW_NEIGHBOR(n2, 5, Nil, 7);
+  const auto &n3 = triangulator._lowNeighbors[3];
+  EXPECT_LOW_NEIGHBOR(n3, 2, Nil, Nil);
+  const auto &n4 = triangulator._lowNeighbors[4];
+  EXPECT_LOW_NEIGHBOR(n4, 1, Nil, 3);
+
+  // check regions
+  const auto &s0 = triangulator._regions[0];
+  EXPECT_REGION(s0, 17, Inf, 0, Inf, Inf, 8, 9, Inf, Inf, 0);
+  const auto &s1 = triangulator._regions[1];
+  EXPECT_REGION(s1, 13, 4, 2, 1, 3, 7, 7, 6, 6, 1);
+  const auto &s2 = triangulator._regions[2];
+  EXPECT_REGION(s2, 3, 3, Inf, Inf, Inf, Inf, Inf, 5, 3, 0);
+  const auto &s3 = triangulator._regions[3];
+  EXPECT_REGION(s3, 5, 4, 3, 3, Inf, 2, 2, 9, 9, 0);
+  const auto &s4 = triangulator._regions[4];
+  EXPECT_REGION(s4, 12, 1, 2, Inf, 1, 5, 5, 8, 8, 0);
+  const auto &s5 = triangulator._regions[5];
+  EXPECT_REGION(s5, 15, 2, 3, Inf, 2, 2, 2, 4, 4, 0);
+  const auto &s6 = triangulator._regions[6];
+  EXPECT_REGION(s6, 20, 1, 4, 1, 4, 1, 1, 10, 10, 1);
+  const auto &s7 = triangulator._regions[7];
+  EXPECT_REGION(s7, 14, 2, 3, 2, 3, Nil, Nil, 1, 1, 1);
+  const auto &s8 = triangulator._regions[8];
+  EXPECT_REGION(s8, 22, 0, 1, Inf, 0, 4, 4, 0, 0, 0);
+  const auto &s9 = triangulator._regions[9];
+  EXPECT_REGION(s9, 18, 0, 4, 4, Inf, 3, 3, 0, 0, 0);
+  const auto &s10 = triangulator._regions[10];
+  EXPECT_REGION(s10, 21, 0, 1, 0, 4, 6, 6, Nil, Nil, 1);
+
+  auto results = triangulator.Triangulate();
+  EXPECT_EQ(results.size(), 3);
 }
 
 // TEST(BasicTest, BoatCCWMergeLeft2SameYs)
